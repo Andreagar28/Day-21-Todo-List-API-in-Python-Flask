fe2a9bac22f06ea6e7b684870b6b064dc9a570db
Andreagar28 <agarchig28@gmail.com>
2021-05-19 16:22:18 +0000

    comienzo de ejercicio GET API

--- src/main.py
+++ src/main.py
@@ -8,7 +8,7 @@ from flask_swagger import swagger
 from flask_cors import CORS
 from utils import APIException, generate_sitemap
 from admin import setup_admin
-from models import db, User
+from models import db, User, Todo
 #from models import Person
 
 app = Flask(__name__)
@@ -30,16 +30,43 @@ def handle_invalid_usage(error):
 def sitemap():
     return generate_sitemap(app)
 
+#-------Get de todos los usuarios
 @app.route('/user', methods=['GET'])
-def handle_hello():
+def get_user():
+    all_users = User.get_all() 
 
-    response_body = {
-        "msg": "Hello, this is your GET /user response "
-    }
+    return jsonify(all_users), 200
 
-    return jsonify(response_body), 200
+#-------Get de todos los emails de los usuarios
+@app.route('/user/<email>', methods=['GET'])
+def get_users_by_email(email):
+    user = User.get_by_email(email) 
+    print(jsonify(user))
 
-# this only runs if `$ python src/main.py` is executed
+    return jsonify(user), 200
+
+
+#aqui viene la lista de las funciones creadas en la tabla Todo
+
+#-------Get de todos los mi tabla Todo
+@app.route('/todo', methods=['GET'])
+def get_todo():
+    all_todos = Todo.get_all() 
+
+    return jsonify(all_todos), 200
+
+#-------Get de todos los user_id de mi tabla Todo
+@app.route('/todo/<user_id>', methods=['GET'])
+def get_todo_by_user_id(user_id):
+    todo = Todo.get_by_user_id(user_id) 
+
+    return jsonify(todo), 200
+
+
+
+
+
+# this only runs if '$ python src/main.py' is executed
 if __name__ == '__main__':
     PORT = int(os.environ.get('PORT', 3000))
     app.run(host='0.0.0.0', port=PORT, debug=False)
--- src/models.py
+++ src/models.py
@@ -1,19 +1,76 @@
 from flask_sqlalchemy import SQLAlchemy
+from sqlalchemy import Column, ForeignKey, Integer, String, Table
 
 db = SQLAlchemy()
 
 class User(db.Model):
+    __tablename__ = 'user'
     id = db.Column(db.Integer, primary_key=True)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
+    have_task = db.relationship('Todo', lazy=True)
 
+    #Función para el Print 
     def __repr__(self):
-        return '<User %r>' % self.username
+        return f'User {self.email}, {self.id}'
 
-    def serialize(self):
+    #Función to dict 
+    def to_dict(self):
         return {
-            "id": self.id,
-            "email": self.email,
-            # do not serialize the password, its a security breach
+            'id': self.id, 
+            'email': self.email
         }
+
+    #Función para mostrar todos los usurios
+    def get_all():
+        users = User.query.all()
+        users_dictionary = list(map(lambda x: x.to_dict(), users))
+        return users_dictionary
+    #Función para mostar los emails de los usuarios
+    def get_by_email(email):
+        user = User.query.filter_by(email=email)
+        user_dictionary = list(map(lambda x: x.to_dict(), user))
+        return user_dictionary
+
+
+class Todo(db.Model):
+    __tablename__ = 'todo'
+    id = db.Column(db.Integer, primary_key=True)
+    tag = db.Column(db.String(250), unique=False, nullable=False)
+    done = db.Column(db.Boolean(False), unique=False, nullable=False)
+    user_id = db.Column(db.Integer, ForeignKey('user.id'))
+    
+# se realiza el "__repr__" para que nos sirvan los "print()"
+    def __repr__(self):
+        return f'Todo {self.id}, {self.user_id}, {self.tag}, {self.done}'
+
+# se hace una funcion "to_dict()" para que muestre en la api de manera: clave valor (tipo diccionario)
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'user_id': self.user_id,
+            'tag': self.tag,
+            'done': self.done
+        }
+
+#ahora tienes que crear tu funcion "get_all()" para que puedas mostrar todos en la tabla de Todo
+
+    def get_all():
+        todos = Todo.query.all()
+        todos_dictionary = list(map(lambda x: x.to_dict(), todos))
+        return todos_dictionary
+
+    def get_by_user_id(user_id):
+        todo = Todo.query.filter_by(user_id=user_id)
+        todo_dictionary = list(map(lambda x: x.to_dict(), todo))
+        return todo_dictionary
+    
+    # def get_by_tag(tag):
+    #     tag_todo = Todo.query.filter_by(tag=tag)
+    #     tag_todo_dictionary = list(map(lambda x: x.to_dict(), tag_todo))
+    #     return tag_todo_dictionary
+
+    # def get_by_done(done):
+    #     done_todo = Todo.query.filter_by(done=done)
+    #     done_todo_dictionary = list(map(lambda x: x.to_dict(), done_todo))
+    #     return done_todo_dictionary
+
